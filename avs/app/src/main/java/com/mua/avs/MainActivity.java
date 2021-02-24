c93368614575c0b286775ee0488dad84033ec5bd
maifeeulasad <maifeeulasad@gmail.com>
2021-02-24 12:10:59 +0600

    draw over other app, permission check and grant

--- avs/app/src/main/java/com/mua/avs/MainActivity.java
+++ avs/app/src/main/java/com/mua/avs/MainActivity.java
@@ -1,9 +1,12 @@
 package com.mua.avs;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,8 +20,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initView();
+        initPermission();
         initListener();
         init();
+    }
+
+    void initPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(Settings.canDrawOverlays(this)){
+                return;
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+        }else{
+            Toast
+                    .makeText(
+                            this,
+                            "Please go to Settings -> Apps -> Draw over other apps and add this",
+                            Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 
     void initView() {
